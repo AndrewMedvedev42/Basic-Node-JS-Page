cf19fad93b10e0176e8055ac341c6d38b5d1ba87
AndrewMedvedev42 <sanjino56@gmail.com>
2021-07-16 15:54:50 +0300

    deploy of initial files

---
+++ index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
+const { RSA_NO_PADDING } = require('constants')
+    http.createServer((req,res)=>{
+        if(req.url === "/"){
+
+            fs.readFile(path.join(__dirname, "index.html"), (err, data)=>{
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.write(data)
+                res.end()  
+            })
+        }
+        if(req.url === "/another-page"){
+            fs.readFile(path.join(__dirname, "another-page.html"), (err, data)=>{
+                res.writeHead(200,{"Content-Type":"text/html"})
+                res.write(data)
+                res.end()  
+            })
+        }
+    }).listen(5000, console.log("Server is launched and running"))
